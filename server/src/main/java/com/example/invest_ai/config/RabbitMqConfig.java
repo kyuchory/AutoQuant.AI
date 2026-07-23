6bb186869f7d40a8eb8346c78752a4053f7990a2
kyuchory <sls789456@naver.com>
2026-07-23 23:45:17 +0900

    feat: 리포트 큐 및 바인딩 추가 - 리포트 처리를 위한 큐와 라우팅 키 설정

--- server/src/main/java/com/example/invest_ai/config/RabbitMqConfig.java
+++ server/src/main/java/com/example/invest_ai/config/RabbitMqConfig.java
@@ -15,6 +15,8 @@ public class RabbitMqConfig {
     public static final String EXCHANGE_NAME = "invest-network";
     public static final String NEWS_QUEUE = "news-queue";
     public static final String NEWS_ROUTING_KEY = "news.#";
+    public static final String REPORT_QUEUE = "report-queue";
+    public static final String REPORT_ROUTING_KEY = "report.#";
 
     @Bean
     public TopicExchange investExchange() {
@@ -34,6 +36,18 @@ public class RabbitMqConfig {
         return BindingBuilder.bind(newsQueue())
                 .to(investExchange())
                 .with(NEWS_ROUTING_KEY);
+    }
+
+    @Bean
+    public Queue reportQueue() {
+        return new Queue(REPORT_QUEUE, true);
+    }
+
+    @Bean
+    public Binding reportBinding() {
+        return BindingBuilder.bind(reportQueue())
+                .to(investExchange())
+                .with(REPORT_ROUTING_KEY);
     }
 
     @Bean
